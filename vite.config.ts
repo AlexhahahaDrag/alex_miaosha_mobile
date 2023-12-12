5813d7a4086f8c9b876b0b12ce2b85934a5596e4
majf <734663446@qq.com>
2023-12-12 15:22:52 +0800

    修改测试-3

--- vite.config.ts
+++ vite.config.ts
@@ -57,4 +57,21 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // 调整包的大小
+    rollupOptions: {
+      output: {
+        // 最小化拆分包
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+        // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+        entryFileNames: 'assets/js/[name].[hash].js', // 用于命名代码拆分时创建的共享块的输出命名
+        chunkFileNames: 'assets/js/[name].[hash].js', // 用于输出静态资源的命名，[ext]表示文件扩展名
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]'
+      }
+    }
+  },
 })
