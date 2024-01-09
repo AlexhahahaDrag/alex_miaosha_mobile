b113f7a653bf7154d98cbcd88511ad2bf8d98344
majf <majf@emrubik.com>
2024-01-09 16:17:39 +0800

    添加unplugin-auto-import和unplugin-vue-components -3（未测试打包）

--- src/views/finance/financeAnalysis/detail/incomeAnalysis.vue
+++ src/views/finance/financeAnalysis/detail/incomeAnalysis.vue
@@ -17,7 +17,6 @@
 </template>
 
 <script lang="ts" setup>
-import pieChart from "@/views/model/pieChart/index.vue";
 import { getIncomeAndExpense } from '@/api/finance/financeAnalysis/index';
 import { showNotify } from 'vant';
 import { ItemInfo } from "./common";
