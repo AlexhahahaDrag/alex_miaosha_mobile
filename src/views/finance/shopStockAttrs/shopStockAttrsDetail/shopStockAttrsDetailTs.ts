1728a3c6936ac43c4c47e38aa41f7403a67189b0
majf <734663446@qq.com>
2024-04-16 20:11:59 +0800

    添加库存商品属性模块

---
+++ src/views/finance/shopStockAttrs/shopStockAttrsDetail/shopStockAttrsDetailTs.ts
@@ -0,0 +1,47 @@
+export const label = reactive({
+    stockId: '库存id',
+    attrCode: '商品属性编码',
+    attrName: '商品属性名称',
+    attrValue: '商品属性值',
+    isValid: '状态',
+    description: '描述',
+});
+
+export const rulesRef = reactive({
+    stockId: [
+        {
+            required: true,
+            message: label.stockId + '不能为空！',
+        },
+    ],
+    attrCode: [
+        {
+            required: true,
+            message: label.attrCode + '不能为空！',
+        },
+    ],
+    attrName: [
+        {
+            required: true,
+            message: label.attrName + '不能为空！',
+        },
+    ],
+    attrValue: [
+        {
+            required: true,
+            message: label.attrValue + '不能为空！',
+        },
+    ],
+    isValid: [
+        {
+            required: true,
+            message: label.isValid + '不能为空！',
+        },
+    ],
+    description: [
+        {
+            required: true,
+            message: label.description + '不能为空！',
+        },
+    ],
+});
